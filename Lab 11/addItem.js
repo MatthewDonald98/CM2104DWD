ac88d1a1dc4485a22c01113c9f9fca1f3154166e
MatthewDonald98 <matthewdonald@rocketmail.com>
2018-03-08 09:44:38 +0000

    Add files via upload

---
+++ Lab 11/addItem.js
@@ -0,0 +1,17 @@
+$(function(){
+  //documentReady
+  alert("document ready");
+
+//listener tied to the form submit button
+  $('#searchform').submit(funtion(){
+    //call the function
+    addItemToList("example item");
+    //stops the page reloading
+    return false;
+  });
+});
+
+function addItemToList (item){
+  //adds the item into the list
+  $('#results').append("<li>" + item + "</li>");
+}
---
+++ Lab 11/printJSON.js
@@ -0,0 +1,33 @@
+$(function(){
+  //documentReady
+  alert("document ready");
+
+//listener tied to the form submit button
+  $('#searchform').submit(function() {
+    //gets the items in the search term box
+    var searchterms = $("#searchterms").val();
+    //call the function
+    getResultsFromOMDB(searchterms);
+    //stops the page reloading
+    return false;
+  });
+});
+
+function getResultsFromOMDB(searchterms) {
+  //call youtube API using AJAX
+  //build url for the request
+  var url = "http://www.omdbapi.com/?apikey=c619ea91&s=" + searchterms;
+  //use jquery json shortcut
+  $.getJSON(url, function(jsondata) {
+    //handle the results
+    printJSON(jsondata);
+  });
+
+}
+
+function printJSON(jsondata){
+  //prints the JSON to the screen
+  var normal = JSON.stringify(jsondata);
+  $('#resultsbox').append("<p>" + normal + "</p>");
+
+}
